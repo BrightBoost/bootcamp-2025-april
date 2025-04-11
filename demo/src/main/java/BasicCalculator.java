96ff0fcdadc42559818f661c773d37d8d9d06a97
maaikevanputten <maaike.vanputten@brightboost.nl>
2025-04-11 20:59:16 +0200

    basic calculator

---
+++ demo/src/main/java/BasicCalculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        // create scanner
+        Scanner reader = new Scanner(System.in);
+
+        // 2 floating point nrs
+        double num1 = reader.nextDouble();
+        double num2 = reader.nextDouble();
+
+        reader.nextLine();
+
+        // asks operation
+        System.out.println("Do you want to add, subtract, multiply or divide?");
+        String operation = reader.nextLine();
+        System.out.println("You've chosen " + operation);
+
+        // multiply
+        double result = num1 * num2;
+        System.out.println("Result: " + result);
+    }
+}
