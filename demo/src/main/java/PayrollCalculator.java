b0689417829b1c8964c63c073fbfee61c26e3bdb
maaikevanputten <maaike.vanputten@brightboost.nl>
2025-04-11 22:12:59 +0200

    exercise 4 payroll

---
+++ demo/src/main/java/PayrollCalculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PayrollCalculator {
+    public static void main(String[] args) {
+        // create a scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        // get name
+        System.out.println("Please enter your name: ");
+        String name = scanner.nextLine();
+
+        // hours worked
+        System.out.println("Enter hours worked, please: ");
+        double hrsWorked = scanner.nextDouble();
+
+        // pay rate
+        System.out.println("Please enter pay rate: ");
+        double payRate = scanner.nextDouble();
+
+        // calc gross pay
+        double grossPay = hrsWorked * payRate;
+
+        // display name + pay
+        System.out.println(name + " has " + grossPay + " as a salary.");
+    }
+}
