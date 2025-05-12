6443e6d548021f38f3abc1fabb9c7ff05ab9e8cc
maaikevanputten <maaike.vanputten@brightboost.nl>
2025-05-12 20:27:10 +0200

    more stuff

--- demo/src/main/java/week6/subpackage/Main.java
+++ demo/src/main/java/week6/subpackage/Main.java
@@ -2,7 +2,7 @@ package week6.subpackage;
 
 import week6.Rabbit;
 
-public class Main {
+public class Main  {
     public static void main(String[] args) {
         Rabbit rabbit = new Rabbit();
         rabbit.setName("Choco");
@@ -11,6 +11,6 @@ public class Main {
         LopRabbit lopRabbit = new LopRabbit();
         lopRabbit.setName("Loppie");
         lopRabbit.attendRabbitShow();
-//        lopRabbit.hide(); not accessible, because protected - so only inside the child class
+//        lopRabbit.hide(); // not accessible, because protected - so only inside the child class
     }
 }
